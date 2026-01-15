4c4c3317b5add05e0823abe0ff6fe75bd511ee83
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-01-15 17:30:31 +0100

    feat: Implemented addstr

--- chararray.c
+++ chararray.c
@@ -1,8 +1,14 @@
 #include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "chararray.h"
+
+static void str_realloc(CharArray *arr) {
+  arr->ptr = (char *)realloc(arr->ptr, sizeof(char) * arr->cap);
+  assert(arr->ptr != NULL);
+}
 
 // general DynArray functions
 int str_init(CharArray *arr, const size_t cap) {
@@ -11,20 +17,32 @@ int str_init(CharArray *arr, const size_t cap) {
   arr->ptr = (char *)malloc(sizeof(char) * cap);
   assert(arr->ptr != NULL);
 
+  arr->ptr[0] = '\0';
+
   return 0;
 } // str_init
 
 int char_append(CharArray *arr, const char c) {
-  if (arr->length == arr->cap) {
+  if (arr->length + 2 >= arr->cap) { // + 2 to count c and '\0'
     arr->cap *= 2;
-    arr->ptr = (char *)realloc(arr->ptr, sizeof(char) * arr->cap);
+    str_realloc(arr);
   }
-  arr->ptr[arr->length] = c;
-  ++arr->length;
+  arr->ptr[arr->length++] = c;
+  arr->ptr[arr->length] = '\0';
   return 0;
 } // char_append
 
 // specific CharArray functions
-int addstr(CharArray *arr, const char *str);
+int addstr(CharArray *arr, const char *str) {
+  if ((arr->cap - arr->length) < strlen(str) + 1) {
+    arr->cap = strlen(str) + 1 + arr->length;
+    str_realloc(arr);
+  }
+
+  strcat(arr->ptr, str);
+  arr->length += strlen(str);
+  arr->ptr[arr->length] = '\0';
+  return 0;
+} // addstr
 int setstr(CharArray *arr, const char *str);
 char *getstr(CharArray *arr);
--- main.c
+++ main.c
@@ -2,11 +2,27 @@
 
 #include "chararray.h"
 
+void str_printInfo(CharArray *arr) {
+  printf("Length: %zu\nCapacity: %zu\nContent: %s\n", arr->length, arr->cap,
+         arr->ptr);
+}
+
 int main() {
   CharArray strTest;
   str_init(&strTest, 10);
-  char_append(&strTest, 'H');
+  printf("\nInit String\n");
+  str_printInfo(&strTest);
 
-  printf("Pos0: %c\n", strTest.ptr[0]);
+  char_append(&strTest, 'H');
+  printf("\nAppend char\n");
+  str_printInfo(&strTest);
+
+  addstr(&strTest, "ello World");
+  printf("\nAppend String with reallocation\n");
+  str_printInfo(&strTest);
+
+  printf("\nAppend char with reallocation\n");
+  char_append(&strTest, '!');
+  str_printInfo(&strTest);
   return 0;
 }
